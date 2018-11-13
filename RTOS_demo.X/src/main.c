2f4f4de5f165ab370d51577e04ef57a53efa43de
KruseAaron <45019022+KruseAaron@users.noreply.github.com>
2018-11-13 17:10:08 -0600

    Update main.c

--- RTOS_demo.X/src/main.c
+++ RTOS_demo.X/src/main.c
@@ -13,7 +13,7 @@
 //RTOS tasks include
 #include "app.h"
 #include "taskBlinky.h"
-
+#include "heartbeat.h"
 /*****************************************************************************
 
     Private Macros
@@ -31,8 +31,7 @@
     Private functions prototype
 
  *****************************************************************************/
-static void taskHeartbeat_Init(unsigned portBASE_TYPE uxPriority);
-static void taskHeartbeat_Execute(void);
+
 static void prvMainCoRoutine(CoRoutineHandle_t xHandle, unsigned portBASE_TYPE uxIndex);
 
 
@@ -100,27 +99,6 @@ static void prvMainCoRoutine(CoRoutineHandle_t xHandle, unsigned portBASE_TYPE u
     crEND();
 }
 
-// *****************************************************************************
-// Private functions implementation
-// *****************************************************************************
-
-static void taskHeartbeat_Init(unsigned portBASE_TYPE uxNumberToCreate)
-{
-    xCoRoutineCreate(prvMainCoRoutine, crf_HEART_BEAT_PRIORITY, crfFLASH_INDEX);
-}
-
-/*
-    Process the heartbeat. This is done in the main event loop (as
-    opposed to an interrupt) so we can see if the App has locked up.
-*/
-static void taskHeartbeat_Execute(void)
-{
-    portENTER_CRITICAL();
-    {
-        IO_RB15_Toggle();        //Toggle signal
-    }
-    portEXIT_CRITICAL();
-}
 
 /*******************************************************************************
  End of File
