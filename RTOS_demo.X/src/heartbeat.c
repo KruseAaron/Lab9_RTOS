997f7766206e52339825a269a2d087287da9b0f3
KruseAaron <45019022+KruseAaron@users.noreply.github.com>
2018-11-13 17:18:57 -0600

    Update heartbeat.c

--- RTOS_demo.X/src/heartbeat.c
+++ RTOS_demo.X/src/heartbeat.c
@@ -3,7 +3,7 @@
 // Private functions implementation
 // *****************************************************************************
 
-static void taskHeartbeat_Init(unsigned portBASE_TYPE uxNumberToCreate)
+void taskHeartbeat_Init(unsigned portBASE_TYPE uxNumberToCreate)
 {
     xCoRoutineCreate(prvMainCoRoutine, crf_HEART_BEAT_PRIORITY, crfFLASH_INDEX);
 }
@@ -12,7 +12,7 @@ static void taskHeartbeat_Init(unsigned portBASE_TYPE uxNumberToCreate)
     Process the heartbeat. This is done in the main event loop (as
     opposed to an interrupt) so we can see if the App has locked up.
 */
-static void taskHeartbeat_Execute(void)
+void taskHeartbeat_Execute(void)
 {
     portENTER_CRITICAL();
     {
